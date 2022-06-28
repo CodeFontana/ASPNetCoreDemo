aaca37327e4689adea9f49c909eb12d32d7aaff7
Brian Fontana <brian@codefoxtrot.com>
2022-06-28 00:56:09 -0400

    Update .NET, nuget packages, and launch settings
    Use Kestrel profile for each project and launch on a distinct port. Also update to .NET 6.0 and bring in the latest nuget packages.

--- BlazorClientDemo/Program.cs
+++ BlazorClientDemo/Program.cs
@@ -18,7 +18,7 @@ namespace BlazorClientDemo
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { 
-                BaseAddress = new Uri("https://localhost:44378") 
+                BaseAddress = new Uri("https://localhost:5001") 
             });
 
             await builder.Build().RunAsync();
