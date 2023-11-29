da200b8bb7ee28c91da15c92fc7346cbd6ee9f2b
Brian Fontana <brian.fontana@allscripts.com>
2023-11-29 16:04:26 -0500

    Begin add Order page

---
+++ BlazorSSRDemo/Models/OrderUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSSRDemo.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Name of the Order")]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
+        [Required]
+        public string? OrderName { get; set; }
+    }
+}
