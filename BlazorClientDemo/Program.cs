a0c5a721aca01e6c4c04ef10af101a4167b6e268
Brian Fontana <brian@codefoxtrot.com>
2025-01-26 16:53:43 -0500

    Simplify Program.cs

--- BlazorClientDemo/Program.cs
+++ BlazorClientDemo/Program.cs
@@ -1,20 +1,14 @@
+using BlazorClientDemo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazorClientDemo;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-public class Program
+builder.Services.AddScoped(sp => new HttpClient
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+    BaseAddress = new Uri("https://localhost:5001")
+});
 
-        builder.Services.AddScoped(sp => new HttpClient { 
-            BaseAddress = new Uri("https://localhost:5001") 
-        });
-
-        await builder.Build().RunAsync();
-    }
-}
+await builder.Build().RunAsync();
