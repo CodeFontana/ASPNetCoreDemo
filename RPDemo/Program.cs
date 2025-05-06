8324dc0f4a27c41e06cfa1e34951b051eae6230f
Brian Fontana <brian@codefoxtrot.com>
2025-05-06 00:15:46 -0400

    Remove startup classes
    Startup classes were so 2021.

--- RPDemo/Program.cs
+++ RPDemo/Program.cs
@@ -1,19 +1,26 @@
+using DataLibrary.Data;
+using DataLibrary.DataAccess;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace RPDemo;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IFoodRepository, FoodRepository>();
+builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+var app = builder.Build();
 
-public class Program
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+app.Run();
